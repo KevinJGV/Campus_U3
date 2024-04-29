f7333659912cb3dfeb301b8ec65b8d3aac8035dd
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2024-04-29 16:59:06 -0500

    feat:

---
+++ Ciclo_Python/Exercises/Python Exs2.py
@@ -0,0 +1,41 @@
+# EJ1
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+lista = []
+
+while True:
+    inp = input("Ingrese materia (valor numerico para cerrar): ")
+    try:
+        if type(int(inp)) == int:
+            break
+    except ValueError:
+        lista.append(inp)
+result = [f"{lista.index(i)}) {i}" for i in lista] if len(lista) > 0 else "No hay materias :("
+print("Materias almacenadas:\n", [i for i in result])
+
+# EJ2
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+
+# EJ3
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
+
+# EJ4
+# Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+# EJ5
+# Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+
+# EJ6
+# Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
+
+# EJ7
+# Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo
+
+# EJ8
+# Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal.
+
+# EJ9
+# Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+
+# EJ10
+# Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media.
