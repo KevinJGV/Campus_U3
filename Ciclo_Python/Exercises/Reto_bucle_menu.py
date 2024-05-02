aa6619edb616334423ebe709a21d75d4ff77bb77
Johan González <85241471+KevinJGV@users.noreply.github.com>
2024-05-02 15:46:53 -0500

    refactor: Uso de tupla para almacenar opciones de menu
    Descubrí la forma de almacenar funciones en estructuras de datos y como llamarlas

--- Ciclo_Python/Exercises/Reto_bucle_menu.py
+++ Ciclo_Python/Exercises/Reto_bucle_menu.py
@@ -1,23 +1,36 @@
-def es_primo(a):
+def es_primo():
     counter = 0
+    a = int(input("> Comprobador de numero primo\n Número a evaluar?\n> "))
     for i in range(1, a+1):
         if a % i == 0:
             counter += 1
     return f"'{a}' Es primo" if counter == 2 else f"'{a}' No es primo"
 
 
-def es_par(a):
+def es_par():
+    a = int(input("> Comprobador de numero par\n Número a evaluar?\n> "))
     if a % 2 == 0:
         return f"Numero {a} es par."
     else:
         return f"Numero {a} es impar."
 
 
-def media(a):
-    return sum(a) / len(a)
+def media():
+    lista = []
+    list_len = 0
+    print("> Calculador de media\n")
+    while True:
+        a = int(input("Números a evaluar? (-1 para terminar)\n> "))
+        if a == -1:
+            list_len = len(lista)
+            break
+        else:
+            lista.append(a)
+    return sum(lista) / list_len
 
 
-def es_xz(a):
+def es_xz():
+    a = input("> Finder de xz\n Texto a evaluar?\n> ")
     letters = ["x", "z"]
     flag = False
     for letter in letters:
@@ -27,38 +40,13 @@ def es_xz(a):
     return f"'{a}' contiente la letra {letters[0]} o {letters[1]}" if flag else f"'{a}' NO contiente la letra {letters[0]} o {letters[1]}"
 
 
-lista = []
+menu = es_primo, es_par, media, es_xz
 
 while True:
-    op = input("=" * 6 + " Menu multiproposito " + "=" * 6 +
-               "\n\n1 - Comprobador de numero primo\n2 - Comprobador de numero par\n3 - Calculador de media\n4 - Finder de xz\n5 - Salir\n> ")
-    if op == "1":
-        prompt = int(
-            input("> Comprobador de numero primo\n Número a evaluar?\n> "))
-        print(es_primo(prompt))
-        input()
-    elif op == "2":
-        prompt = int(
-            input("> Comprobador de numero par\n Número a evaluar?\n> "))
-        print(es_par(prompt))
-        input()
-    elif op == "3":
-        print("> Calculador de media\n")
-        while True:
-            prompt = int(input("Números a evaluar? (-1 para terminar)\n> "))
-            if prompt == -1:
-                break
-            else:
-                lista.append(prompt)
-        print(media(lista))
-        input()
-    elif op == "4":
-        prompt = input("> Finder de xz\n Texto a evaluar?\n> ")
-        print(es_xz(prompt))
-        input()
-    elif op == "5":
-        print("> Salir")
-        break
+    op = int(input("=" * 6 + " Menu multiproposito " + "=" * 6 +
+             "\n\n1 - Comprobador de numero primo\n2 - Comprobador de numero par\n3 - Calculador de media\n4 - Finder de xz\n5 - Salir\n> "))
+    if op != 5:
+        print(menu[op-1]())
     else:
-        print("Opcion Erronea.\n\n")
-        input()
+        print("> Saliendo...")
+        break
