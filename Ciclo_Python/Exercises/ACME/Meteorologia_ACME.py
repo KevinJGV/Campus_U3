e249df62d60a8506b6185cd42492f6d38b95ba88
Johan González <85241471+KevinJGV@users.noreply.github.com>
2024-05-08 17:32:33 -0500

    feat: inicio menu

--- Ciclo_Python/Exercises/ACME/Meteorologia_ACME.py
+++ Ciclo_Python/Exercises/ACME/Meteorologia_ACME.py
@@ -1,5 +1,7 @@
-import defs
+from defs import *
+ruta = "Exercises/ACME/db.csv"
+# just_open = open(ruta)
+# res = leer_csv(ruta)
+# asd = csv_to_dict(just_open)
 
-res = defs.leer_txt("Ciclo_Python/Exercises/ACME/db.csv")
-for i in res:
-    print(i)
+menu(leer_csv, csv_to_dict, ruta=ruta)
