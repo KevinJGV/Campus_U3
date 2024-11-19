15d2061d70f8bb38ee5f0ab4345eca1b692f53e9
Kevin Gonzalez <kejogo.dev@outlook.com>
2024-11-19 17:15:12 -0500

    actividad de practica

---
+++ Ciclo_Java/SortArray/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] nomes = {"Pedro", "Mano", "bien", "alicia", "vin"};
+
+        Arrays.sort(nomes);
+
+//        for (String nome : nomes) {
+//            System.out.println(nome);
+//        }
+
+        Arrays.sort(nomes, new Comparator<String>() {
+            @Override
+            public int compare(String o1,String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
